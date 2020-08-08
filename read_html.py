563af44369b9d9d40a109a032194c7b558d0c281
Coutjj <coutinholima.j@gmail.com>
2020-08-08 00:12:14 -0300

    esqueci o headless :)

--- read_html.py
+++ read_html.py
@@ -8,7 +8,7 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox") # linux only
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 # chrome_options.headless = True # also works
 #driver = webdriver.Chrome(options=chrome_options)
 
